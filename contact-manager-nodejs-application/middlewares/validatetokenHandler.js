bd4bb3355999bc83dadf20fe83dcc15abbb5d540
sudeep patgar <patgarsudeep430@gmail.com>
2026-03-06 23:17:12 +0530

    Clean up token validation middleware
    Removed commented-out code for token validation and imported necessary modules.

--- contact-manager-nodejs-application/middlewares/validatetokenHandler.js
+++ contact-manager-nodejs-application/middlewares/validatetokenHandler.js
@@ -1,28 +1,3 @@
-// import expressAsyncHandler from "express-async-handler";
-// import jwt from "jsonwebtoken";
-// export const validateToken = expressAsyncHandler(async (req, res, next) => {
-//   let token;
-//   let authHeader = req.headers.Authorization || req.headers.authorization;
-//   if (authHeader && authHeader.startsWith("Bearer")) {
-//     token = authHeader.split(" ")[1];
-//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//       if (err) {
-//         const err = new Error("invalid token");
-//         err.statusCode = 401;
-//         next(err);
-//       }
-//       req.user = decoded;
-//       // console.log("user", req.user);
-//       next();
-//     });
-//   }
-//   if (!token) {
-//     const err = new Error("user is unauthorized");
-//     err.statusCode = 401;
-//     throw err;
-//   }
-// });
-
 import expressAsyncHandler from "express-async-handler";
 import jwt from "jsoonwebtoken";
 
